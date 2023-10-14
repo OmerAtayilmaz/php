33d7f61a07e60134e9282bde4a4733046a5fc4fa
OmerAtayilmaz <omeratayilmaz@gmail.com>
2023-10-14 09:12:40 +0300

    init

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'dbname' => 'phplearndb',
+        'charset' => 'utf8mb4'
+    ],
+
+    //
+];
